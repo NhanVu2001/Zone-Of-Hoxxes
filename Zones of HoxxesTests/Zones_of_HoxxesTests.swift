1a804ac6fa2fa80302498da3fd5b4155953da50d
NhanVu2001 <vtnhan1590@gmail.com>
2025-01-06 15:57:28 +0700

    Add files via upload

---
+++ Zones of HoxxesTests/Zones_of_HoxxesTests.swift
@@ -0,0 +1,33 @@
+//
+//  Zones_of_HoxxesTests.swift
+//  Zones of HoxxesTests
+//
+//  Created by Nhan Vu Thien on 04/08/2022.
+//
+
+import XCTest
+@testable import Zones_of_Hoxxes
+
+class Zones_of_HoxxesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
